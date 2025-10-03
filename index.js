134dabf8aea441e35754748c8ef3fc929e567f97
Krishna Kumar <106340@ondirect.local>
2025-10-03 15:07:53 +0530

    Describe what you changed

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const http = require('http');
 
-const PORT = process.env.PORT || 3000;
+const PORT = 5000;
 
 const server = http.createServer((req, res) => {
   res.end('Hello from Node.js on Docker with GitHub Actions!');
 });
 
 server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Note Krishna Server running on http://localhost:${PORT}`);
 });
