094b645d3b462f3954422f71757455a1093fe9a3
Krishna Kumar <106340@ondirect.local>
2025-10-03 16:19:11 +0530

    chang in http

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const PORT = 5000;
 
 const server = http.createServer((req, res) => {
-  res.end('Hello from Node.js on Docker with GitHub Actions!');
+  res.end('hi Krishna Hello from Node.js on Docker with GitHub Actions!');
 });
 
 server.listen(PORT, () => {
